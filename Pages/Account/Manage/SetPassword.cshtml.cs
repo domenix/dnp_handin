e71281babbc5266b4dd35dae44618c31b9183f2e
domenix <kd.dominik@gmail.com>
2018-05-27 15:36:41 +0200

    Changed password requirements

--- Pages/Account/Manage/SetPassword.cshtml.cs
+++ Pages/Account/Manage/SetPassword.cshtml.cs
@@ -32,7 +32,7 @@ namespace VIAMovies.Pages.Account.Manage
         public class InputModel
         {
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
             [DataType(DataType.Password)]
             [Display(Name = "New password")]
             public string NewPassword { get; set; }
--- Pages/Account/ResetPassword.cshtml.cs
+++ Pages/Account/ResetPassword.cshtml.cs
@@ -29,7 +29,7 @@ namespace VIAMovies.Pages.Account
             public string Email { get; set; }
 
             [Required]
-            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 4)]
             [DataType(DataType.Password)]
             public string Password { get; set; }
 
