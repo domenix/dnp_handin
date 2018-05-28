26873116e45d7448dc3343f812b5d390a5886844
Unknown <grzechu.cat@gmail.com>
2018-05-29 01:10:12 +0200

    More small changes to the UI

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -4,10 +4,20 @@ $(function() {
     // page is now ready, initialize the calendar...
   
     $('#calendar').fullCalendar({
-      // put your options and callbacks here
+      eventClick: function(calEvent, jsEvent, view) {
+
+        alert('Event: ' + calEvent.title);
+        alert('Coordinates: ' + jsEvent.pageX + ',' + jsEvent.pageY);
+        alert('View: ' + view.name);
+    
+        // change the border color just for fun
+        $(this).css('border-color', 'red');
+    
+      },
       defaultView: 'basicWeek',
       height: 650,
-      firstday: 1
+      firstDay: 1
     })
-  
+    $('document').ready(function(){console.log(json2);});
+    
   });
