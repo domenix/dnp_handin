5bafcf6004cb37a01ffb5e270855428fe15c42dc
Unknown <grzechu.cat@gmail.com>
2018-05-30 21:23:47 +0200

    Inputs for reservation

--- Pages/CreateReservation.cshtml.cs
+++ Pages/CreateReservation.cshtml.cs
@@ -1,18 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using VIAMovies.Models;
+using VIAMovies.Services;
 
 namespace VIAMovies.Pages
 {
     public class CreateReservationModel : PageModel
     {
-        public string Message { get; set; }
+      
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
 
-        public void OnGet()
+        public CreateReservationModel(
+            UserManager<User> userManager,
+            SignInManager<User> signInManager)
         {
-            Message = "This will return the available movies, hopefully";
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public Task<bool> IsAdmin(User user)
+        {
+            return _userManager.IsInRoleAsync(user, "Admin");
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return RedirectToPage("/Index");
+            }
+            else if (!await IsAdmin(user))
+            {
+                return RedirectToPage("/Index");
+            }
+
+            return Page();
         }
     }
 }
