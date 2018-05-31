5cc49a8fe0afc6c0746e56817dbd2631df01bc36
GrzeWho <grzechu.cat@gmail.com>
2018-05-31 18:22:51 +0200

    Fixed permissions

--- Pages/CreateReservation.cshtml.cs
+++ Pages/CreateReservation.cshtml.cs
@@ -27,21 +27,12 @@ namespace VIAMovies.Pages
             _signInManager = signInManager;
         }
 
-        public Task<bool> IsAdmin(User user)
-        {
-            return _userManager.IsInRoleAsync(user, "Admin");
-        }
-
         public async Task<IActionResult> OnGetAsync()
         {
             var user = await _userManager.GetUserAsync(User);
             if (user == null)
             {
-                return RedirectToPage("/Index");
-            }
-            else if (!await IsAdmin(user))
-            {
-                return RedirectToPage("/Index");
+                return RedirectToPage("/Account/Login");
             }
 
             return Page();
